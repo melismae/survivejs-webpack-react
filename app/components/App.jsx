e85f68d1f04f8bd3ff02d265150ff8d39ac8a27a
Melissa Smith <melissa@Melissas-MacBook-Pro.local>
2016-06-03 12:58:57 -0700

    react functionality

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,8 +1,53 @@
+import uuid from 'node-uuid';
 import React from 'react';
-import Note from './Note.jsx';
+import Notes from './Notes.jsx';
 
 export default class App extends React.Component {
+  //defines the initial state of the component
+  //gets called when the component is instantiated initially
+  constructor(props) {
+    //if props aren't passed to super this.props won't get set
+    super(props);
+    //initial data definitions as state
+    this.state = {
+      notes: [
+        {
+          id: uuid.v4(),
+          task: 'Do Something Cool'
+        },
+        {
+          id: uuid.v4(),
+          task: 'Make the Codes work'
+        },
+        {
+          id: uuid.v4(),
+          task: 'Walk dog'
+        },
+        {
+          id: uuid.v4(),
+          task: 'Eat Donuts'
+        }
+      ]
+    }
+  }
+
+  addNote  = () => {
+    this.setState({
+      notes: this.state.notes.concat([{
+        id: uuid.v4(),
+        task: 'New task'
+      }])
+    });
+  }
+
   render() {
-    return <Note />;
+    const notes = this.state.notes;
+
+    return (
+      <div>
+        <button onClick={this.addNote}>+</button>
+        <Notes notes={notes} />
+      </div>
+    );
   }
 }
--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -1,3 +1,64 @@
 import React from 'react';
 
-export default () => <div>Learn Webpack like hella good.</div>;
+export default class Note extends React.Component {
+  constructor(props) {
+    super(props);
+
+    //Track editing state
+    this.state = {
+      editing: false
+    };
+  }
+  render() {
+    //Render the component differently based on state
+    if(this.state.editing) {
+      return this.renderEdit();
+    }
+
+    return this.renderNote();
+  }
+  renderEdit = () => {
+    console.log("renderEdit");
+    return <input type="text"
+      ref={
+        (e) => e ? e.selectionState = this.props.task.length : null
+      }
+      autoFocus={true}
+      defaultValue={this.props.task}
+      onBlur={this.finishEdit}
+      onKeyPress={this.checkEnter} />
+  }
+  renderNote = () => {
+    console.log("renderNote");
+
+    // if the user clicks a normal note, trigger editing logic
+    return <div onClick={this.edit}>{this.props.task}</div>;
+  };
+  edit = () => {
+    console.log("edit");
+    //Enter edit mode
+    this.setState({
+      editing: true
+    });
+  };
+  checkEnter = (e) => {
+    console.log("checkEnter");
+    // The user hit enter -> finish up
+    if(e.key === 'Enter') {
+      this.finishEdit(e);
+    }
+  };
+  finishEdit = (e) => {
+    console.log("finishEdit");
+
+    const value = e.target.value;
+
+    if(this.props.onEdit) {
+      this.props.onEdit(value);
+      //exit edit mode
+      this.setState({
+        editing: false
+      });
+    }
+  };
+}
